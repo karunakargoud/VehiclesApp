ec43392da18dc8acfe06f2b73ae7d78bfcc7d478
karunakargoud <karunakargoud1116@gmail.com>
2024-12-01 14:40:50 +0530

    FirstCommit

---
+++ VehiclesApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            vehicles MyVehicle= new vehicles();
+            vehicles Car = new car();
+            vehicles Bicycle=new Bicycle(); 
+            vehicles Boat=new Boat();
+
+            MyVehicle.ShowVehiclesTypes();
+            Car.ShowVehiclesTypes();
+            Bicycle.ShowVehiclesTypes();
+            Boat.ShowVehiclesTypes();
+        }
+    }
+}
---
+++ VehiclesApp/vehicles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclesApp
+{
+    internal class vehicles
+    {
+        public virtual void ShowVehiclesTypes()
+        {
+            Console.WriteLine("Type of Vehicle");
+           
+        }
+    }
+    class car : vehicles 
+    {
+        public override void ShowVehiclesTypes()
+        {
+            Console.WriteLine("Type of Vehicle - On the road");
+           
+        }
+    }
+    class Bicycle : vehicles
+    { 
+        public override void ShowVehiclesTypes()
+        {
+            Console.WriteLine("Type of Vehicle - on the road ");
+          
+        }
+    }
+    class Boat : vehicles
+    {
+        public override void ShowVehiclesTypes()
+        {
+            Console.WriteLine("Type of Vehicle - on the surface of the water ");
+            Console.ReadKey(); 
+        }
+     
+    }
+
+
+
+
+
+}
